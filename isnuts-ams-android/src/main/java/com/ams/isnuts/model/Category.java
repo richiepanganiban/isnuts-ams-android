195788495f2e532bfc272ca22634baefcbb3a4ba
Philip Roger Seralde <philip.seralde@orangeandbronze.com>
2011-11-13 17:56:32 +0800

    created Category model

---
+++ isnuts-ams-android/src/main/java/com/ams/isnuts/model/Category.java
@@ -0,0 +1,74 @@
+package com.ams.isnuts.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Category {
+
+	@SerializedName("id")
+	private long id;
+
+	@SerializedName("name")
+	private String name;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Category other = (Category) obj;
+		if (id != other.id) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Category [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
