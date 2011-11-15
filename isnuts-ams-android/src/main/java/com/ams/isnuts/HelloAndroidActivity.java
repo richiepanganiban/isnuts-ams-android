d853348790cff5103a16495076dd1c390cd0506d
richiepanganiban <richie.panganiban@orangeandbronze.com>
2011-11-15 19:55:07 +0800

    Initial dao commit

--- isnuts-ams-android/src/main/java/com/ams/isnuts/HelloAndroidActivity.java
+++ isnuts-ams-android/src/main/java/com/ams/isnuts/HelloAndroidActivity.java
@@ -2,28 +2,37 @@ package com.ams.isnuts;
 
 import android.app.Activity;
 import android.os.Bundle;
+
+import com.ams.isnuts.dao.SqliteServiceDao;
+import com.ams.isnuts.model.Service;
+
 import de.akquinet.android.androlog.Log;
-import com.ams.isnuts.R;
 
 public class HelloAndroidActivity extends Activity {
 
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after
-     * previously being shut down then this Bundle contains the data it most
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
+	private SqliteServiceDao sqliteServiceDao = new SqliteServiceDao(this);
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Initializes the logging
         Log.init();
-
         // Log a message (only on dev platform)
         Log.i(this, "onCreate");
-
         setContentView(R.layout.main);
+        
+        Service service = new Service();
+        service.setTitle("Service # 1");
+        sqliteServiceDao.insert(service);
+        
+        
+        
+    }
+    
+    @Override
+    public void onDestroy() {
+    	super.onDestroy();
+    	sqliteServiceDao.close();
     }
 
 }
