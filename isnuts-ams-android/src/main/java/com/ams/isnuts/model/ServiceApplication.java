275953e28bf241a3d6f7c696b1f54bd65b585d4c
Philip Roger Seralde <philip.seralde@orangeandbronze.com>
2011-11-18 14:55:33 +0800

    made inputparams LinkedList rather of ArrayList

--- isnuts-ams-android/src/main/java/com/ams/isnuts/model/ServiceApplication.java
+++ isnuts-ams-android/src/main/java/com/ams/isnuts/model/ServiceApplication.java
@@ -1,10 +1,15 @@
 package com.ams.isnuts.model;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import com.google.gson.annotations.SerializedName;
 
 public class ServiceApplication {
+
+	@SerializedName("id")
+	private long id;
+
 	@SerializedName("activePromo")
 	private boolean activePromo;
 
@@ -24,10 +29,18 @@ public class ServiceApplication {
 	private boolean appendMobileToServiceNumber;
 
 	@SerializedName("keywordItems")
-	private List<InputParams> keywordItems;
+	private LinkedList<InputParams> keywordItems;
 
 	@SerializedName("categories")
 	private List<Category> categories;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
 
 	public boolean isActivePromo() {
 		return activePromo;
@@ -77,11 +90,11 @@ public class ServiceApplication {
 		this.appendMobileToServiceNumber = appendMobileToServiceNumber;
 	}
 
-	public List<InputParams> getKeywordItems() {
+	public LinkedList<InputParams> getKeywordItems() {
 		return keywordItems;
 	}
 
-	public void setKeywordItems(List<InputParams> keywordItems) {
+	public void setKeywordItems(LinkedList<InputParams> keywordItems) {
 		this.keywordItems = keywordItems;
 	}
 
@@ -91,6 +104,72 @@ public class ServiceApplication {
 
 	public void setCategories(List<Category> categories) {
 		this.categories = categories;
+	}
+
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (activePromo ? 1231 : 1237);
+		result = prime * result + (appendMobileToServiceNumber ? 1231 : 1237);
+		result = prime * result + ((categories == null) ? 0 : categories.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((keywordItems == null) ? 0 : keywordItems.hashCode());
+		result = prime * result + ((serviceNumber == null) ? 0 : serviceNumber.hashCode());
+		result = prime * result + ((serviceType == null) ? 0 : serviceType.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ServiceApplication other = (ServiceApplication) obj;
+		if (activePromo != other.activePromo)
+			return false;
+		if (appendMobileToServiceNumber != other.appendMobileToServiceNumber)
+			return false;
+		if (categories == null) {
+			if (other.categories != null)
+				return false;
+		} else if (!categories.equals(other.categories))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id != other.id)
+			return false;
+		if (keywordItems == null) {
+			if (other.keywordItems != null)
+				return false;
+		} else if (!keywordItems.equals(other.keywordItems))
+			return false;
+		if (serviceNumber == null) {
+			if (other.serviceNumber != null)
+				return false;
+		} else if (!serviceNumber.equals(other.serviceNumber))
+			return false;
+		if (serviceType == null) {
+			if (other.serviceType != null)
+				return false;
+		} else if (!serviceType.equals(other.serviceType))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
 	}
 
 	@Override
@@ -115,5 +194,7 @@ public class ServiceApplication {
 		builder.append("]");
 		return builder.toString();
 	}
+	
+	
 
 }
