50bed8dc871c42ab0b88632075dcb5d4f36683a7
richiepanganiban <richie.panganiban@orangeandbronze.com>
2011-11-21 19:34:30 +0800

    Dummy View

--- isnuts-ams-android/src/main/java/com/ams/isnuts/HelloAndroidActivity.java
+++ isnuts-ams-android/src/main/java/com/ams/isnuts/HelloAndroidActivity.java
@@ -1,14 +1,12 @@
 package com.ams.isnuts;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.ams.isnuts.dao.SqliteServiceApplicationDao;
-import com.ams.isnuts.model.Category;
-import com.ams.isnuts.model.InputParams;
 import com.ams.isnuts.model.ServiceApplication;
 
 import de.akquinet.android.androlog.Log;
@@ -17,7 +15,8 @@ public class HelloAndroidActivity extends Activity {
 
 	private SqliteServiceApplicationDao sqliteServiceDao = new SqliteServiceApplicationDao(this);
 	
-    @Override
+
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Initializes the logging
@@ -26,35 +25,41 @@ public class HelloAndroidActivity extends Activity {
         Log.i(this, "onCreate");
         setContentView(R.layout.main);
         
-        ServiceApplication serviceApplication = new ServiceApplication();
-        serviceApplication.setServiceApplicationId(1000L);
-        serviceApplication.setTitle("Unlitxt 20");
-        serviceApplication.setDescription("P20.00 lang, unli ka na!");
-        serviceApplication.setServiceType("TEXT");
-        serviceApplication.setServiceNumber("8888");
-        serviceApplication.setActivePromo(true);
-        serviceApplication.setAppendMobileToServiceNumber(false);
+//        List<ServiceApplication> pulledServices = sqliteServiceDao.getAllApplicationServices();
+//        ServiceApplication pulledService = pulledServices.get(0);
+//        Log.d("After pulling");
         
-        InputParams inputParams = new InputParams();
-        inputParams.setItemType("Unlitxt For 1 Day Keyword");
-        inputParams.setLiteralValue("Unlitxt20");
-        List<InputParams> inputParamsList = new ArrayList<InputParams>();
-        inputParamsList.add(inputParams);
-        serviceApplication.setKeywordItems(inputParamsList);
-        
-        Category category = new Category();
-        category.setId(2000L);
-        category.setName("Txt promos");
-        List<Category> categoriesList = new ArrayList<Category>();
-        categoriesList.add(category);
-        serviceApplication.setCategories(categoriesList);
-        
-//        sqliteServiceDao.insert(serviceApplication);
-        List<ServiceApplication> pulledServices = sqliteServiceDao.getAllApplicationServices();
-        
+//        TextView idTextView = (TextView) findViewById(R.id.serviceAppId);
+//        TextView titleTextView = (TextView) findViewById(R.id.title);
+//        TextView descTextView = (TextView) findViewById(R.id.description);
+//        TextView serviceTypeTextView = (TextView) findViewById(R.id.serviceType);
+//        TextView serviceNumberTextView = (TextView) findViewById(R.id.serviceNumber);
+//        TextView activePromoTextView = (TextView) findViewById(R.id.activePromo);
+//        TextView appendTextView = (TextView) findViewById(R.id.appendToMobile);
+//        TextView categoryTextView = (TextView) findViewById(R.id.category);
+//        TextView keywordTextView = (TextView) findViewById(R.id.keyword);
+//
+//        idTextView.setText(pulledService.getServiceApplicationId()+"");
+//        titleTextView.setText(pulledService.getTitle());
+//        descTextView.setText(pulledService.getDescription());
+//        serviceTypeTextView.setText(pulledService.getServiceType());
+//        serviceNumberTextView.setText(pulledService.getServiceNumber());
+//        activePromoTextView.setText(pulledService.isActivePromo()+"");
+//        appendTextView.setText(pulledService.isAppendMobileToServiceNumber()+"");
+//        categoryTextView.setText(pulledService.getCategories().get(0).getName());
+//        keywordTextView.setText(pulledService.getKeywordItems().get(0).getLiteralValue());
         
         
     }
+	
+    public SqliteServiceApplicationDao getSqliteServiceDao() {
+		return sqliteServiceDao;
+	}
+
+	public void setSqliteServiceDao(SqliteServiceApplicationDao sqliteServiceDao) {
+		this.sqliteServiceDao = sqliteServiceDao;
+	}
+
     
     @Override
     public void onDestroy() {
