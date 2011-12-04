da2d5238bb666b37486ed1911b2f27665cd9364a
unknown <ichie@ichie-pc.(none)>
2011-12-05 00:29:25 +0800

    Added targetMobileNumber and changed toString() of ServiceApplication
    Conflicts:
    
    	isnuts-ams-android/src/main/java/com/ams/isnuts/model/ServiceApplication.java

--- isnuts-ams-android/src/main/java/com/ams/isnuts/model/ServiceApplication.java
+++ isnuts-ams-android/src/main/java/com/ams/isnuts/model/ServiceApplication.java
@@ -7,9 +7,9 @@ import com.google.gson.annotations.SerializedName;
 
 public class ServiceApplication {
 
-	@SerializedName("id")
-	private long id;
-
+	@SerializedName("serviceApplicationId")
+	private long serviceApplicationId;
+	
 	@SerializedName("activePromo")
 	private boolean activePromo;
 
@@ -34,12 +34,14 @@ public class ServiceApplication {
 	@SerializedName("categories")
 	private List<Category> categories;
 
-	public long getId() {
-		return id;
+	private String targetMobileNumber;
+	
+	public long getServiceApplicationId() {
+		return serviceApplicationId;
 	}
-
-	public void setId(long id) {
-		this.id = id;
+	
+	public void setServiceApplicationId(long serviceApplicationId) {
+		this.serviceApplicationId = serviceApplicationId;
 	}
 
 	public boolean isActivePromo() {
@@ -105,96 +107,17 @@ public class ServiceApplication {
 	public void setCategories(List<Category> categories) {
 		this.categories = categories;
 	}
-
-	
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (activePromo ? 1231 : 1237);
-		result = prime * result + (appendMobileToServiceNumber ? 1231 : 1237);
-		result = prime * result + ((categories == null) ? 0 : categories.hashCode());
-		result = prime * result + ((description == null) ? 0 : description.hashCode());
-		result = prime * result + (int) (id ^ (id >>> 32));
-		result = prime * result + ((keywordItems == null) ? 0 : keywordItems.hashCode());
-		result = prime * result + ((serviceNumber == null) ? 0 : serviceNumber.hashCode());
-		result = prime * result + ((serviceType == null) ? 0 : serviceType.hashCode());
-		result = prime * result + ((title == null) ? 0 : title.hashCode());
-		return result;
+	public String getTargetMobileNumber() {
+		return targetMobileNumber;
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ServiceApplication other = (ServiceApplication) obj;
-		if (activePromo != other.activePromo)
-			return false;
-		if (appendMobileToServiceNumber != other.appendMobileToServiceNumber)
-			return false;
-		if (categories == null) {
-			if (other.categories != null)
-				return false;
-		} else if (!categories.equals(other.categories))
-			return false;
-		if (description == null) {
-			if (other.description != null)
-				return false;
-		} else if (!description.equals(other.description))
-			return false;
-		if (id != other.id)
-			return false;
-		if (keywordItems == null) {
-			if (other.keywordItems != null)
-				return false;
-		} else if (!keywordItems.equals(other.keywordItems))
-			return false;
-		if (serviceNumber == null) {
-			if (other.serviceNumber != null)
-				return false;
-		} else if (!serviceNumber.equals(other.serviceNumber))
-			return false;
-		if (serviceType == null) {
-			if (other.serviceType != null)
-				return false;
-		} else if (!serviceType.equals(other.serviceType))
-			return false;
-		if (title == null) {
-			if (other.title != null)
-				return false;
-		} else if (!title.equals(other.title))
-			return false;
-		return true;
+	public void setTargetMobileNumber(String targetMobileNumber) {
+		this.targetMobileNumber = targetMobileNumber;
 	}
 
 	@Override
 	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("ServiceApplication [activePromo=");
-		builder.append(activePromo);
-		builder.append(", serviceType=");
-		builder.append(serviceType);
-		builder.append(", title=");
-		builder.append(title);
-		builder.append(", description=");
-		builder.append(description);
-		builder.append(", serviceNumber=");
-		builder.append(serviceNumber);
-		builder.append(", appendMobileToServiceNumber=");
-		builder.append(appendMobileToServiceNumber);
-		builder.append(", keywordItems=");
-		builder.append(keywordItems);
-		builder.append(", categories=");
-		builder.append(categories);
-		builder.append("]");
-		return builder.toString();
+		return title;
 	}
-	
-	
 
 }
